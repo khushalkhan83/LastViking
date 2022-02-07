aaa421988ae5c84c9b2c0c02eba8acde24e25d74
Gleb <dev.gleb@retrostylegames.com>
2022-02-07 16:26:45 +0200

    [added] Sandbox

---
+++ UnityProject/Assets/_Sandbox/Misc/GameObjectSwithcer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectSwithcer : MonoBehaviour
+{
+    public void Switch(GameObject target) => target.SetActive(!target.activeSelf);
+}
