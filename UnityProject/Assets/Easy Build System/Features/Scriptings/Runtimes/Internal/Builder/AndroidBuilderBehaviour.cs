b98099f3ebabc36ecc64a0aac46211d50db35e56
Gleb <dev.gleb@retrostylegames.com>
2022-02-07 15:47:38 +0200

    [added] Easy Build System

---
+++ UnityProject/Assets/Easy Build System/Features/Scriptings/Runtimes/Internal/Builder/AndroidBuilderBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using EasyBuildSystem.Runtimes.Extensions;
+using EasyBuildSystem.Runtimes.Internal.Builder;
+using EasyBuildSystem.Runtimes.Internal.Managers;
+using UnityEngine;
+
+[AddComponentMenu("Easy Build System/Features/Builders Behaviour/Android Builder Behaviour")]
+public class AndroidBuilderBehaviour : BuilderBehaviour
+{
+    public override void Start()
+    {
+        base.Start();
+
+        if (BuildManager.Instance == null)
+            return;
+
+        if (BuildManager.Instance.PartsCollection != null)
+            SelectPrefab(BuildManager.Instance.PartsCollection.Parts[0]);
+    }
+}
