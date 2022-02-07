53b3a97f776c200d50d02f96a4cca41d7b429e36
Gleb <dev.gleb@retrostylegames.com>
2022-02-07 16:13:10 +0200

    [added] Post Effects

---
+++ UnityProject/Assets/PostEffects/Grayscale/GrayscalePostEffect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GrayscalePostEffect : MonoBehaviour
+{
+    #region Data
+#pragma warning disable 0649
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float _blend;
+    [SerializeField] private Shader _shaderGrayscale;
+
+#pragma warning restore 0649
+    #endregion
+
+    public float Blend => _blend;
+    public Shader ShaderGrayscale => _shaderGrayscale;
+    private Material material;
+
+    public void SetBlend(float blend)
+    {
+        _blend = Mathf.Clamp01(blend);
+    }
+
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(ShaderGrayscale);
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (Blend == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        material.SetFloat("_Blend", Blend);
+        Graphics.Blit(source, destination, material);
+    }
+}
