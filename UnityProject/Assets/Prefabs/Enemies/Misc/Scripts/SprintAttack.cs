73bb291ddad31d6f1fcc6193a1d292d6da902861
Gleb <dev.gleb@retrostylegames.com>
2022-02-07 16:13:48 +0200

    [added] Prefabs

---
+++ UnityProject/Assets/Prefabs/Enemies/Misc/Scripts/SprintAttack.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using CodeStage.AntiCheat.ObscuredTypes;
+using Game.AI;
+using Game.Models;
+using UnityEngine;
+
+namespace Game.AI.BehaviorDesigner
+{
+    public class SprintAttack : MonoBehaviour
+    {
+        public event Action<Target> OnKillTarget;
+
+        #region Data
+    #pragma warning disable 0649
+
+        [SerializeField] ObscuredFloat _damage;
+        [SerializeField] BehaviorTree _behaviorTree;
+        [SerializeField] Collider _collider;
+        
+    #pragma warning restore 0649
+        #endregion
+
+        private SharedGameObject _targetVaeiable;
+
+        private PlayerHealthModel PlayerHealthModel => ModelsSystem.Instance._playerHealthModel;
+
+
+        private void OnEnable() 
+        {
+            _behaviorTree.RegisterEvent("StartSprintAttack", OnStartSprintAttack);
+            _behaviorTree.RegisterEvent("StopSprintAttack", OnStopSprintAttack);
+            _targetVaeiable = (SharedGameObject)_behaviorTree.GetVariable("Target");
+        }
+
+        private void OnDisable() 
+        {
+            _behaviorTree.UnregisterEvent("StartSprintAttack", OnStartSprintAttack);
+            _behaviorTree.UnregisterEvent("StopSprintAttack", OnStopSprintAttack);
+        }
+
+        private void OnStartSprintAttack()
+        {
+            print("StartSprintAttack");
+            _collider.enabled = true;
+        }
+
+        private void OnStopSprintAttack()
+        {
+            print("StopSprintAttack");
+            _collider.enabled = false;
+        }
+
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            Target target = other.GetComponent<Target>();
+
+            if (target == null) 
+                return;
+
+            GameObject targetGO = (GameObject)_targetVaeiable.GetValue();
+
+            if(targetGO == null || targetGO.GetComponent<Target>() != target)
+                return;
+
+            HandleDamage();
+
+            bool targetIsPlayer = target.ID == TargetID.Player;
+
+            HandleTargetIsDead();
+                
+            void HandleDamage()
+            {
+                var damagable = target.GetComponentInParent<IDamageable>();
+
+                if (damagable != null)
+                    damagable.Damage(_damage);
+            }
+
+            void HandleTargetIsDead()
+            {
+                IHealth healthInTarget = targetIsPlayer ? PlayerHealthModel : target.GetComponentInParent<IHealth>();
+
+                if (healthInTarget == null) return;
+
+                if (healthInTarget.IsDead)
+                {
+                    _behaviorTree.SendEvent("KillTarget");
+                    OnKillTarget?.Invoke(target);
+                }
+            }
+
+            _collider.enabled = false;
+        }
+    }
+}
---
+++ UnityProject/Assets/Prefabs/Enemies/Readme.txt
@@ -0,0 +1,16 @@
+prefabs inheretence hierarchy
+
+enemy_crab_base
+	-> enemy_crab_boss
+	-> enemy_crab_normal
+
+enemy_skeleton_regular_base
+	-> enemy_skeleton_warrior
+	-> enemy_skeleton_light
+		-> enemy_skeleton_super_light
+
+enemy_skeleton_archer_base
+	-> enemy_skeleton_archer
+
+enemy_zombie_base
+
