39547e60a091dce9d058ac931ba44180dfdd382f
Gleb <dev.gleb@retrostylegames.com>
2022-02-08 14:37:55 +0200

    [added] build pipeline

---
+++ UnityProject/Assets/ScriptableObjects/Scripts/BuildPipeline/Editor/BuildPipelineWindow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using Sirenix.OdinInspector.Editor;
+
+namespace BuildAutomation
+{
+    public class BuildPipelineWindow : OdinEditorWindow
+    {
+        [MenuItem("EditorTools/Build/Build Pipeline")]
+        public static void OpenWindow()
+        {
+            GetWindow<BuildPipelineWindow>().Show();
+        }
+
+        protected override object GetTarget()
+        {
+            return BuildPipeline.Instance;
+        }
+
+        protected override void OnGUI()
+        {
+            try
+            {
+                base.OnGUI(); 
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+    }
+}
