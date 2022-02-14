5b518d040240d492f3cbf119f401734c825a0678
Gleb <dev.gleb@retrostylegames.com>
2022-02-14 11:30:43 +0200

    [added] quality settings to Intro scene

---
+++ UnityProject/Assets/Scripts/Game/Controllers/Enviroments/Activation/IntroActivationController.cs
@@ -0,0 +1,37 @@
+﻿using Game.Controllers;
+using Game.Models;
+using UnityEngine;
+
+public class IntroActivationController : ActivationObjectsController
+{
+    [SerializeField] private QualityModel qualityModel;
+    protected override void ChangeActivationObjects(QualityConfig qualityConfig)
+    {
+
+    }
+
+    protected override void InitActivationObjects(QualityConfig qualityConfig)
+    {
+        qualityModel.SetQuality(qualityConfig.QuaityID);
+    }
+
+    private void OnEnable()
+    {
+        qualityModel.OnChangeQuality += OnChangeQuality;
+    }
+
+    private void Start()
+    {
+        InitActivationObjects(qualityModel.QualityConfig);
+    }
+
+    private void OnDisable()
+    {
+        qualityModel.OnChangeQuality -= OnChangeQuality;
+    }
+
+    private void OnChangeQuality()
+    {
+        ChangeActivationObjects(qualityModel.QualityConfig);
+    }
+}
