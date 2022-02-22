071caa5d19565dfad102f3331b5cf29f0c985d1f
Viktor <dev.vik@retrostylegames.com>
2022-02-22 10:36:21 +0200

    [added] AnimationStopper component

---
+++ UnityProject/Assets/_Sandbox/AnimationStopper/AnimationStopper.cs
@@ -0,0 +1,28 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class AnimationStopper : MonoBehaviour
+{
+    [SerializeField] private bool _enableAnimatorOnChange;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+
+        TrackAnimatorEnabledChanged();
+    }
+
+    private void TrackAnimatorEnabledChanged()
+    {
+        this.ObserveEveryValueChanged(x => _animator.enabled).Subscribe(x =>
+        {
+            HandleAnimatorEnabledChanged();
+        })/*.AddTo(this)*/;
+    }
+
+    private void HandleAnimatorEnabledChanged()
+    {
+        _animator.enabled = _enableAnimatorOnChange;
+    }
+}
